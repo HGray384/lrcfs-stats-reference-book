3ef62c6f98038389f8920d138ecf4d279ccdd599
Harry Gray <h_g93@hotmail.co.uk>
2020-10-05 14:47:48 +0100

    Update view_home.R
    changed url to correct survey

--- interactive-shiny/views/view_home.R
+++ interactive-shiny/views/view_home.R
@@ -2,7 +2,7 @@ tabHome = tabItem(tabName = "tabHome",
                   fluidRow(
                     box(title="Welcome",
                         p("Thanks for stopping by!"),
-                        HTML('<iframe src="https://dundee.onlinesurveys.ac.uk/stats-book-internal-feedback" width="100%" height="800" style="border:0px"></iframe>')
+                        HTML('<iframe src="https://dundee.onlinesurveys.ac.uk/r-shiny-app-feedback" width="100%" height="800" style="border:0px"></iframe>')
                     )
                   )
                   
