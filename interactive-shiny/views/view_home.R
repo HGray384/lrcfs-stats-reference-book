4ea1a23baf873ed4040a77b3c1b9470a3147e355
HGray384 <h.w.gray@dundee.ac.uk>
2021-02-22 11:31:22 +0000

    fixed URL to book
    #20 fixed using link within HTML()

--- interactive-shiny/views/view_home.R
+++ interactive-shiny/views/view_home.R
@@ -5,7 +5,7 @@ tabHome = tabItem(tabName = "tabHome",
                         p("The application uses interactive examples so that you can explore how different values and assumptions can affect probabilistic and statistical conclusions."),
                         p("On the tab to the left of the application you can find the different examples. Within each tab there are some sub-tabs, which demonstrate different ideas using similar scenarios."),
                         p("You can use any example in any order that you prefer, but if you are unsure where to begin then start with the top one and work your way down the list of tabs."),
-                        p("If you would like to learn more about the ideas introduced here then you can read about them in our <a href='https://lrcfs.dundee.ac.uk/lr-book/'>interactive probability and statistics book</a>."),
+                        p(HTML("If you would like to learn more about the ideas introduced here then you can read about them in our <a href='https://lrcfs.dundee.ac.uk/lr-book/'>interactive probability and statistics book</a>.")),
                         p("We hope that you enjoy learning using this app!")
                     )
                   )
