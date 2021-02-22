ee7b9c2c5353d11cf0d03dd8e970143cf715479d
Roy Mudie <>
2021-02-22 14:09:46 +0000

    Added missing gdtools package to list of required packages

--- code/server-config.R
+++ code/server-config.R
@@ -1,3 +1,7 @@
 #This is a server specific configuration file
 #Update the SHINY_HOST property to work with development, test and production environments
-SHINY_HOST = "http://127.0.0.1:5555"
+SHINY_HOST = "http://127.0.0.1:5555" #development
+
+#SHINY_HOST = "https://lrcfs.dundee.ac.uk/apps/interactive-lr"           #Production Server
+#SHINY_HOST = "https://lrcfs-shiny.test.dundee.ac.uk/apps/interactive-lr" #Test Server
+#SHINY_HOST = "http://127.0.0.1:5555" #development                        #Example development path
