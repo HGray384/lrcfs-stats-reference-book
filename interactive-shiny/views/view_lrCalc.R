7c225bb9d7dc665e50ce72d3ce221ffe19d21d28
HGray384 <h.w.gray@dundee.ac.uk>
2021-03-24 16:32:59 +0000

    mathjax fix

--- interactive-shiny/views/view_lrCalc.R
+++ interactive-shiny/views/view_lrCalc.R
@@ -3,11 +3,12 @@ tabLRCalc = tabItem(tabName = "tabLRCalc",
                              box(title="LR calculator", width=12,
                                  p("In this example we demonstrate how the LR is calculated based upon its underlying conditional probabilities."),
                                  p("The LR depends upon three key elements: "),
+                                 withMathJax(),
                                  tags$div(
                                    tags$ul(
-                                     tags$li("E: evidence,"),
-                                     tags$li("H_p: prosecution hypothesis for the evidence,"),
-                                     tags$li("H_d: defence hypothesis for the evidence. ")
+                                     tags$li("\\(E\\): evidence,"),
+                                     tags$li("\\(H_p\\): prosecution hypothesis for the evidence,"),
+                                     tags$li("\\(H_d\\): defence hypothesis for the evidence. ")
                                    )
                                  )
                              )
@@ -15,12 +16,12 @@ tabLRCalc = tabItem(tabName = "tabLRCalc",
                     fluidRow(
                       box(title="Conditional probabilities", width=12,
                           column(width=6,
-                                 sliderInput("lrCalc_probHp", "Probability for E conditioned on H_p", min=0, max=1,
+                                 numericInput("lrCalc_probHp", "Probability for \\(E\\) conditioned on \\(H_p\\)", min=0, max=1,
                                              value=0.1, step=0.01)
                           ),
                           column(width=6,
-                                 sliderInput("lrCalc_probHd", "Probability for E conditioned on H_d", min=0, max=1,
-                                             value=0.1, step=0.01)
+                                 numericInput("lrCalc_probHd", "Probability for \\(E\\) conditioned on \\(H_d\\)", min=0, max=1,
+                                             value=0.0001, step=0.01)
                           ),
                           column(width=12,
                                  uiOutput("display_lrCalc_likelihoodRatioCalcs")
