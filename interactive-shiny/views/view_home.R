9d8353fa59ea27ea0907744d2922f9c4f53329ed
HGray384 <h.w.gray@dundee.ac.uk>
2021-04-09 01:32:36 +0100

    Update view_home.R
    added research and development info

--- interactive-shiny/views/view_home.R
+++ interactive-shiny/views/view_home.R
@@ -6,7 +6,18 @@ tabHome = tabItem(tabName = "tabHome",
                         p("On the tab to the left of the application you can find the different examples. Within each tab there are some sub-tabs, which demonstrate different ideas using similar scenarios."),
                         p("You can use any example in any order that you prefer, but if you are unsure where to begin then start with the top one and work your way down the list of tabs."),
                         p(HTML("If you would like to learn more about the ideas introduced here then you can read about them in our <a href='https://lrcfs.dundee.ac.uk/lr-book/'>interactive probability and statistics book</a>.")),
-                        p("We hope that you enjoy learning using this app!")
+                        p("We hope that you enjoy learning using this app.")
+                    )
+                  )
+                  ,
+                  fluidRow(
+                    box(title="Information for research participants", width = 12,
+                        p("If you are a research participant in our study, then please follow the instructions given in the survey. The text boxes at the top of each tab may be minimised to reduce screen clutter by clicking the button in their top right-hand corner."),
+                    )
+                  ),
+                  fluidRow(
+                    box(title = "Development information", width = 12,
+                        p("This application is being developed by Mr Roy Mudie (Application Specialist) and Dr Harry Gray (Harding Fellow) at the Leverhulme Research Centre for Forensic Science (LRCFS). The principal investigator for this project is Professor Niamh Nic Daéid.")  
                     )
                   )
 )
